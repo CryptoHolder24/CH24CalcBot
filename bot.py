3e6eaaf09f5e304427604cda1a5053eab4dbaabc
CryptoHolder24 <icryptoholder24@gmail.com>
2026-05-05 21:52:22 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv("BOT_TOKEN")
+if not TOKEN:
+    raise ValueError("❌ BOT_TOKEN не найден. Проверь .env файл")
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 
